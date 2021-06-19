76cfe27eca05795b3740b0735b20b84c4d9ac7a6
Paul-Glanville <glanville49@googlemail.com>
2021-06-19 22:01:15 +0000

    add form in add_item template to allow user to add items

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 
 
@@ -6,6 +6,16 @@ from .models import Item
 def get_todo_list(request):
     items = Item.objects.all()
     context = {
-        'items': items
+        "items": items
     }
     return render(request, "todo/todo_list.html", context)
+
+
+def add_item(request):
+    if request.method == "POST":
+        name = request.post.get("item_name")
+        done = "done" in request.POST
+        Item.objects.create(name=name, done=done)
+
+        return redirect("get_todo_list")
+    return render(request, "todo/add_item.html")
